75537419f53f66ca3f5823d3e2ffe1c38c4ca362
Alexander Nikitin <al.nikitin@andersenlab.com>
2020-06-29 14:21:53 +0300

    Make news route post data to db

--- routes/news.js
+++ routes/news.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../db-connector/pool');
 
+// [R] - read
 // Public API route
 router.get('/', (req, res) => {
-    pool.query('SELECT * FROM news')
+    pool
+        .query('SELECT * FROM news')
         .then((data) => {
             res.send(data.rows);
         })
@@ -13,9 +15,27 @@ router.get('/', (req, res) => {
         });
 });
 
+// CRUD         methods:
+// C - create   POST
+// R - read     GET
+// U - update   PUT
+// D - delete   DELETE
+
 // Secured route
 router.post('/', (req, res) => {
-    res.send('Posted');
+    const { title, brief_text, detailed_text } = req.body;
+
+    pool
+        .query(
+            'INSERT INTO news (title, brief_text, detailed_text) VALUES ($1, $2, $3)',
+            [ title, brief_text, detailed_text ]
+        )
+        .then(data => {
+            res.send(data.rows)
+        })
+        .catch(err => {
+            res.send({ error: err.message })
+        });
 });
 
 module.exports = router;
