09212f3003ca4cbc8b04f3d9aa727290e0eb5a75
Alexander Nikitin <al.nikitin@andersenlab.com>
2020-06-30 11:06:48 +0300

    Start adding async/await

--- controllers/news-controller.js
+++ controllers/news-controller.js
@@ -1,27 +1,31 @@
 const md5 = require('md5');
 const pool = require('../db-connector/pool');
 
-const newsGet = (req, res) => {
-    pool
-        .query('SELECT * FROM news')
-        .then((data) => {
-            res.send(data.rows);
-        })
-        .catch((err) => {
-            res.send({ error: err.message });
-        });
+const newsGet = async (req, res) => {
+    try {
+        const data = await pool.query('SELECT * FROM news');
+        res.send(data.rows);
+    } catch (err) {
+        res.send({ error: err.message });
+    }
+
+    // pool.query('SELECT * FROM news')
+    //     .then((data) => {
+    //         res.send(data.rows);
+    //     })
+    //     .catch((err) => {
+    //         res.send({ error: err.message });
+    //     });
 };
 
-const newsPost = (req, res) => {
+const newsPost = async (req, res) => {
     const { title, brief_text, detailed_text } = req.body;
-
-    // console.log(req.headers.authorization)
 
     if (!req.headers.authorization) {
         return res.send({ error: 'Unauthorized' })
     }
 
-    const [ encodedPayload, signature ] = req.headers.authorization.replace('Bearer ', '').split('.');
+    const [encodedPayload, signature] = req.headers.authorization.replace('Bearer ', '').split('.');
 
     const secretForToken = 'SECRET_FOR_TOKEN';
     const generatedSignature = md5(encodedPayload + secretForToken); // Second part of the token
@@ -30,17 +34,28 @@ const newsPost = (req, res) => {
         // here we would also check if user from encodedPayload
         // is allowed to post the data
 
-        pool
-            .query(
+        try {
+            const data = await pool.query(
                 'INSERT INTO news (title, brief_text, detailed_text) VALUES ($1, $2, $3)',
-                [ title, brief_text, detailed_text ]
-            )
-            .then(data => {
-                res.send(data.rows)
-            })
-            .catch(err => {
-                res.send({ error: err.message })
-            });
+                [title, brief_text, detailed_text]
+            );
+
+            res.send(data.rows)
+        } catch(err) {
+            res.send({ error: err.message })
+        }
+
+        // pool
+        //     .query(
+        //         'INSERT INTO news (title, brief_text, detailed_text) VALUES ($1, $2, $3)',
+        //         [title, brief_text, detailed_text]
+        //     )
+        //     .then(data => {
+        //         res.send(data.rows)
+        //     })
+        //     .catch(err => {
+        //         res.send({ error: err.message })
+        //     });
     } else {
         res.send({ error: 'Something wrong with auth' })
     }
